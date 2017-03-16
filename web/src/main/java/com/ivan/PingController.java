6586f1b3f71236f2e58467f5768a3e77cd5075ca
IvanStankov <ivan.stankov1992@gmail.com>
2017-03-16 11:21:36 +0600

    cargo deployment plugin

---
+++ web/src/main/java/com/ivan/PingController.java
@@ -0,0 +1,16 @@
+package com.ivan;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/ping")
+public class PingController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String ping() {
+        return "OK";
+    }
+
+}
---
+++ web/src/main/java/com/ivan/configuration/WebApplicationContext.java
@@ -0,0 +1,7 @@
+package com.ivan.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebApplicationContext {
+}

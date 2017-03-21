cd71ebd6b9492ca056984e48b904367ea2a39620
IvanStankov <ivan.stankov1992@gmail.com>
2017-03-22 01:18:57 +0600

    - Serve index.html - Print build finish date

--- web/src/main/java/com/ivan/configuration/MvcContext.java
+++ web/src/main/java/com/ivan/configuration/MvcContext.java
@@ -3,12 +3,17 @@ package com.ivan.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.ivan.web")
-public class MvcContext {
+public class MvcContext extends WebMvcConfigurerAdapter {
 
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/");
+    }
 
 }
